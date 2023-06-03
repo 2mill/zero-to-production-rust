b84bef6cb3ccd8f26bce16ab258c4d43c9770f5a
gd0st <yannick.dorn@gmail.com>
2023-06-02 22:58:56 -0500

    boilerplate hello world

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
-fn main() {
-    println!("Hello, world!");
+//! src/main.rs
+//! [...]
+
+use actix_web::{web, App, HttpRequest, HttpServer, Responder};
+
+async fn greet(req: HttpRequest) -> impl Responder {
+    let name = req.match_info().get("name").unwrap_or("World");
+    format!("Hello {}", &name)
+}
+
+#[tokio::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .route("/", web::get().to(greet))
+            .route("/{name}", web::get().to(greet))
+    })
+    .bind("127.0.0.1:8000")
+    .expect("Started actix web server")
+    .run()
+    .await
 }
